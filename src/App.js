21ece243784c30de2a25683c52367cb72b2dbd07
Rodrigo <fori2001@hotmail.com>
2023-08-28 17:28:30 -0300

    Modulo 01 terminado

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormSignUp from './components/FormSignUp';
+function App() {
+  return (
+    <>
+      <h1>Formulario Registro</h1>
+      <FormSignUp />
+    </>
+  );
+}
+
+export default App;
